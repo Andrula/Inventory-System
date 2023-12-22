6df9fa61066cf1a6a723e09571aec6cbd94da8cd
Andreas Rasmussen <xandreas@live.dk>
2023-12-22 09:29:17 +0100

    -Added interface folder -Added IPcRepository

---
+++ Inventory-System/Data/Repositories/PcRepository.cs
@@ -0,0 +1,37 @@
+﻿using Inventory_System.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_System.Data.Repositories
+{
+    public class PcRepository : IPcRepository
+    {
+        public void Add(pc pc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int pcID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<pc> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public pc GetById(int pcID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(pc pc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
