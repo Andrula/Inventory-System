a35b28aa31804ab9e42849927f4ed5772312a480
Andreas Rasmussen <xandreas@live.dk>
2024-01-25 14:11:13 +0100

    -Added ribbon

---
+++ Inventory-System/Controls/NavigationBar/NavigationBarViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Inventory_System.Common.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Inventory_System.Controls
+{
+    public class NavigationBarViewModel : ViewModelBase
+    {
+        private int _selectedEquipmentIndex = 0;
+
+        public int SelectedEquipmentIndex
+        {
+            get { return _selectedEquipmentIndex; }
+            set
+            {
+                if (_selectedEquipmentIndex != value)
+                {
+                    _selectedEquipmentIndex = value;
+                    RaisePropertyChanged(nameof(SelectedEquipmentIndex));
+                }
+            }
+        }
+
+        // Add a property to get the selected equipment type based on the index
+        public string SelectedEquipmentType
+        {
+            get
+            {
+                string[] equipmentTypes = { "General", "PC", "Tablet", "Phone", "SIM-Card" };
+
+                if (SelectedEquipmentIndex >= 0 && SelectedEquipmentIndex < equipmentTypes.Length)
+                {
+                    return equipmentTypes[SelectedEquipmentIndex];
+                }
+
+                return "General"; // Default to General if index is out of bounds
+            }
+        }
+    }
+}
